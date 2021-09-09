b4b313e0835a85307a6a02b163db9d505d121987
CamdynR <rasque.camdyn@gmail.com>
2021-09-09 13:30:31 -0700

    updated demo #13 for regular expressions

--- regular-expressions/13-indicating-repetition.js
+++ regular-expressions/13-indicating-repetition.js
@@ -2,4 +2,5 @@
 /ab*c/.test('abc'); // true
 /ab*c/.test('abbbc');  // true - we can repeat the b
 /ab*c/.test('bc'); // false - missing the a
-/ab*c/.test('The letters abc are found at the start of the alphabet'); // true - remember we are not showing position here
+/ab*c/.test('The letters abc are found at the start of the alphabet');
+// ^ true - remember we are not showing position here

0193b4b4134a5c3e882c8b3ffecba53443ce20b9
CamdynR <rasque.camdyn@gmail.com>
2021-09-09 14:18:20 -0700

    Fixed line lengths on regex demo #32

--- regular-expressions/32-negative-classing.js
+++ regular-expressions/32-negative-classing.js
@@ -1,3 +1,5 @@
 let fiveDifferentStrings = /[^,]+(,[^,]+){4}/;
-console.log(`"peter, paul, mary, lary" = ${fiveDifferentStrings.test("peter, paul, mary, lary")}`);
-console.log(`"larry, curly, moe, shemp, curly joe" = ${fiveDifferentStrings.test("larry, curly, moe, shemp, curly joe")}`);
+let test1 = fiveDifferentStrings.test('peter, paul, mary, lary');
+let test2 = fiveDifferentStrings.test('larry, curly, moe, shemp, curly joe');
+console.log(`"peter, paul, mary, lary" = ${test1}`);
+console.log(`"larry, curly, moe, shemp, curly joe" = ${test2}`);

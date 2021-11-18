4aeedff81ccab208e2e11908562c7edfacdfe994
Camdyn Rasque <rasque.camdyn@gmail.com>
2021-11-18 15:12:57 -0800

    Added helloWC demo

---
+++ web-components/hello-web-components/HelloWC-v2.js
@@ -0,0 +1,18 @@
+// Extend HTMLElement so we can add on top of it
+class HelloWC extends HTMLElement {
+  constructor() {
+    // Inheret all of the properties of HTMLElement
+    super();
+  }
+
+  render() {
+    this.innerHTML = '<h1>Hello, Web Components!</h1>';
+  }
+}
+
+// Define our web component in the custom element registry so we can use it in our HTML
+customElements.define('hello-wc', HelloWC);
+
+// Add Web Component to the page
+const helloWCdemo = document.createElement('hello-wc');
+document.body.append(helloWCdemo.render());

5ed43f9dfeda362539e2ca731214fc502a53565b
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-07-14 09:45:11 -0700

    Placed init in the correct place for color themes demo

--- css/static-content/color/themes/script.js
+++ css/static-content/color/themes/script.js
@@ -2,35 +2,35 @@
 
 const themes = ['dark', 'forest', 'lightblue', 'maroon', 'light'];
 
-document.addEventListener('DOMContentLoaded', init);
-
-function init() {
-  bindListeners();
-}
-
 function bindListeners() {
-  const btn = document.querySelector('button img');
-  const sel = document.querySelector('select');
-  const body = document.body;
+	const btn = document.querySelector('button img');
+	const sel = document.querySelector('select');
+	const body = document.body;
 
-  btn.addEventListener('click', () => {
-    sel.value = getNextTheme(body);
-    setTheme(sel.value, body);
-  });
+	btn.addEventListener('click', () => {
+		sel.value = getNextTheme(body);
+		setTheme(sel.value, body);
+	});
 
-  sel.addEventListener('input', () => {
-    setTheme(sel.value, body);
-  });
+	sel.addEventListener('input', () => {
+		setTheme(sel.value, body);
+	});
 }
 
 function getNextTheme(body) {
-  const currTheme = body.classList[0];
-  const nextThemeIndex = (themes.indexOf(currTheme) + 1) % 5;
-  return themes[nextThemeIndex];
+	const currTheme = body.classList[0];
+	const nextThemeIndex = (themes.indexOf(currTheme) + 1) % 5;
+	return themes[nextThemeIndex];
 }
 
 function setTheme(theme, body) {
-  let currTheme = body.classList[0];
-  body.classList.remove(currTheme);
-  body.classList.add(theme);
+	let currTheme = body.classList[0];
+	body.classList.remove(currTheme);
+	body.classList.add(theme);
 }
+
+function init() {
+	bindListeners();
+}
+
+init();

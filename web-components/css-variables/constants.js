c33abbeb7b1415e5bba363a9cbd6c89a73b741ec
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-08-09 15:03:15 -0700

    Updated CSS Variables

--- web-components/css-variables/constants.js
+++ web-components/css-variables/constants.js
@@ -1,9 +1,9 @@
 // constants.js
 
-const cssBgColors {
-  '--bg-light-dom': 'rgb(0, 200, 0)',
-  '--bg-light-dom-unedited': 'rgb(0, 200, 0)',
-  '--bg-shadow-dom': 'rgb(255,75,255)',
-  '--bg-Closed': 'rgb(255, 175, 175)',
-  '--bg-Open': '#BFB'
+const cssBgColors = {
+	'--bg-light-dom': 'rgb(0, 200, 0)',
+	'--bg-light-dom-unedited': 'rgb(0, 200, 0)',
+	'--bg-shadow-dom': 'rgb(255,75,255)',
+	'--bg-Closed': 'rgb(255, 175, 175)',
+	'--bg-Open': '#BFB',
 };

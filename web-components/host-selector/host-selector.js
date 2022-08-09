a54dbc45023e9fb6d3b28b109acfd4558b642ead
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-08-09 14:47:31 -0700

    Working on the part attribute demo:

--- web-components/host-selector/host-selector.js
+++ web-components/host-selector/host-selector.js
@@ -1,9 +1,6 @@
 // host-selector.js
 
 class HostSelector extends HTMLElement {
-	// Private property named shadow which will store the closed shadowRoot
-	#closedShadow;
-
 	/**
 	 * Always call super() first thing in your web component constructor().
 	 * super is a reference to the class we're inhereting, and super() calls
@@ -12,45 +9,59 @@ class HostSelector extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-		this.shadowRoot.innerHTML = `
-      <style>
-        * {
-          font-family: sans-serif;
-        }
+	}
 
-        :host {
-          align-items: center;
-          background-color: lightgray;
-          border: 2px solid gray;
-          border-radius: 4px;
-          box-sizing: border-box;
-          color: rgb(118, 118, 118);
-          display: grid;
-          height: 200px;
-          justify-items: center;
-          width: 400px;
-        }
+	/**
+	 * Runs every time the element is inserted into the DOM. Runs once right
+	 * after page load if element is written into the DOM already.
+	 */
+	connectedCallback() {
+		// The markup to attach to the element's shadowRoot
+		let text = document.createElement('p');
+		text.innerHTML = `Everything outside this green square is the light DOM,
+			even though ALL of the styles are written within the shadow DOM, the
+			light DOM is able to be reached using the <code>:host</code> CSS
+			selector.`;
 
-        code {
-          background-color: white;
-          font-family: monospace;
-          font-size: 1.1rem;
-          padding: 1px 4px;
-        }
+		// The styles to attach to the element's shadowRoot
+		let styles = document.createElement('style');
+		styles.innerHTML = `
+			* {
+				font-family: sans-serif;
+			}
 
-        p {
-          background-color: #BFB;
-          border-radius: 3px;
-          color: black;
-          line-height: 1.4;
-          padding: 5px 10px;
-          width: 60%;
-        }
-      </style>
-      <p>Everything outside this green square is the light DOM, even though
-         ALL of the styles are written within the shadow DOM, the light DOM
-         is able to be reached using the <code>:host</code> CSS selector.</p>
-    `;
+			:host {
+				align-items: center;
+				background-color: lightgray;
+				border: 2px solid gray;
+				border-radius: 4px;
+				box-sizing: border-box;
+				color: rgb(118, 118, 118);
+				display: grid;
+				height: 200px;
+				justify-items: center;
+				width: 400px;
+			}
+
+			code {
+				background-color: white;
+				font-family: monospace;
+				font-size: 1.1rem;
+				padding: 1px 4px;
+			}
+
+			p {
+				background-color: #BFB;
+				border-radius: 3px;
+				color: black;
+				line-height: 1.4;
+				padding: 5px 10px;
+				width: 60%;
+			}
+		`;
+
+		// Append the styles and markup to append to the element's shadowRoot
+		this.shadowRoot.append(styles, text);
 	}
 }
 
